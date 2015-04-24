e8dd60ed29898842843b5d1e0ace62cd6a58df94
James <james@changemachine.org>
2015-04-24 15:52:32 -0700

    Re-factor StudentCtrl function into UtilitiesFactory.

--- js/Controllers.js
+++ js/Controllers.js
@@ -1,51 +1,18 @@
-studentAttendance.controller('StudentsCtrl', function StudentsCtrl($scope) {
-    $scope.students = [
-        { name: "Jane Farley", here: false },
-        { name: "Sally Forth", here: false },
-        { name: "River Fjord", here: false },
-        { name: "John Ford", here: false },
-        { name: "Spanky Larsen", here: false },
-        { name: "Gurgen Ratskeller", here: false },
-        { name: "Minka Feldspar", here: false },
-        { name: "Tran Falcon", here: false }
-    ];
-
-    studentList = [];
-
-    $scope.signIn = function(student) {
-      student.here = true;
-      studentList.push(student);
-      console.log(studentList);
-    };
-
-    $scope.signOut = function(student) {
-      student.here = false;
-      studentList.splice(studentList.indexOf(student), 1);
-      console.log(studentList);
-
-    };
+studentAttendance.controller('StudentsCtrl', function StudentsCtrl($scope, UtilitiesFactory) {
+  $scope.students = UtilitiesFactory.students;
+  $scope.UtilitiesFactory = UtilitiesFactory;
 });
+
 
 // =========== ADMIN CONTROL ==============
 
-studentAttendance.controller('AdminCtrl', function AdminCtrl($scope) {
-  $scope.students = [
-      { name: "Jane Farley", here: false },
-      { name: "Sally Forth", here: false },
-      { name: "River Fjord", here: false },
-      { name: "John Ford", here: false },
-      { name: "Spanky Larsen", here: false },
-      { name: "Gurgen Ratskeller", here: false },
-      { name: "Minka Feldspar", here: false },
-      { name: "Tran Falcon", here: false }
-  ];
-
-  studentList = [];
+studentAttendance.controller('AdminCtrl', function AdminCtrl($scope, UtilitiesFactory) {
+  $scope.students = UtilitiesFactory.students;
+  $scope.UtilitiesFactory = UtilitiesFactory;
 
   $scope.addStudent = function(studentName) {
     student = { name: studentName, here: false };
     $scope.students.push(student);
-    console.log(student);
   };
 
   $scope.deleteStudent = function(student) {
--- js/Services.js
+++ js/Services.js
@@ -1,16 +1,44 @@
+studentAttendance.factory('UtilitiesFactory', function() {
+  var factory = {};
 
+  factory.students = [
+      { name: "Jane Farley", here: false },
+      { name: "Sally Forth", here: false },
+      { name: "River Fjord", here: false },
+      { name: "John Ford", here: false },
+      { name: "Spanky Larsen", here: false },
+      { name: "Gurgen Ratskeller", here: false },
+      { name: "Minka Feldspar", here: false },
+      { name: "Tran Falcon", here: false }
+  ];
 
-courseRoster.factory('UtilitiesFactory', function() {
-    var factory = {};
+  // TO SHOW STUDENT BY "PRESENCE/ABSENCE"
+  studentList = [];
 
-    factory.findById = function(collection, id) {
-        for (var i = 0; i < collection.length; i++) {
-            if (collection[i].id == id) {
-                return collection[i];
-            }
-        }
-        return null;
-    };
-    return factory;
+  factory.signIn = function(student) {
+    student.here = true;
+    studentList.push(student);
+  };
 
+  factory.signOut = function(student) {
+    student.here = false;
+    studentList.splice(studentList.indexOf(student), 1);
+
+  };
+  return factory;
 });
+
+// courseRoster.factory('UtilitiesFactory', function() {
+//     var factory = {};
+//
+//     factory.findById = function(collection, id) {
+//         for (var i = 0; i < collection.length; i++) {
+//             if (collection[i].id == id) {
+//                 return collection[i];
+//             }
+//         }
+//         return null;
+//     };
+//     return factory;
+//
+// });
