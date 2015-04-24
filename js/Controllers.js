a09159ce343c12cf291ee4435a8e51eb1f01df08
James <james@changemachine.org>
2015-04-24 13:31:55 -0700

    UI-Router implementation. Start admin editing.

--- js/Controllers.js
+++ js/Controllers.js
@@ -26,40 +26,18 @@ studentAttendance.controller('StudentsCtrl', function StudentsCtrl($scope) {
     };
 });
 
-/*
-========== FIELD TRIPS CONTROLLER ========
-studentRoster.controller('FieldTripsCtrl', function FieldTripsCtrl($scope, StudentsFactory) {
-  $scope.students = StudentsFactory.students;
-  $scope.addStudentWithPermissionSlip = function(student) {
-    student.permissionSlip = true;
-  };
+studentAttendance.controller('AdminCtrl', function AdminCtrl($scope) {
+  $scope.students = [
+      { name: "Jane Farley", here: false },
+      { name: "Sally Forth", here: false },
+      { name: "River Fjord", here: false },
+      { name: "John Ford", here: false },
+      { name: "Spanky Larsen", here: false },
+      { name: "Gurgen Ratskeller", here: false },
+      { name: "Minka Feldspar", here: false },
+      { name: "Tran Falcon", here: false }
+  ];
+
+  studentList = [];
+
 });
-
-========== STUDENTS FACTORY ========
-studentRoster.factory('StudentsFactory', function StudentsFactory() {
-  var factory = {};
-  factory.students = [];
-
-  factory.addStudent = function() {
-    var student = { name: factory.studentName, permissionSlip: false };
-    factory.students.push(student);
-    factory.studentName = null;
-  };
-
-  factory.deleteStudent = function(student) {
-    var index = factory.students.indexOf(student);
-    factory.students.splice(index, 1);
-  };
-  return factory;
-});
-
-
- ========== ALT: COURSE ROSTER APPROACH ========
-courseRoster.controller('StudentsCtrl', function StudentsCtrl($scope, $stateParams, CoursesFactory, UtilitiesFactory) {
-    $scope.course = UtilitiesFactory.findById(CoursesFactory.courses, $stateParams.courseId);
-    $scope.addStudent = function() {
-        $scope.course.students.push({ name: $scope.studentName });
-        $scope.studentName = null;
-    };
-});
-*/
--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-var studentAttendance = angular.module('studentAttendance', []); //[dependencies]
+var studentAttendance = angular.module('studentAttendance', ['ui.router']);
+
+//UI-ROUTER ADD
+studentAttendance.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html",
+    controller: "AdminCtrl"
+  });
+
+  $stateProvider.state('sign-in', {
+    url: "/sign-in",
+    templateUrl: "partials/sign-in.html",
+    controller: "StudentsCtrl"
+  });
+
+  $stateProvider.state('admin', {
+    url: "/admin",
+    templateUrl: "partials/admin.html",
+    controller: "AdminCtrl"
+  });
+
+  // FROM:
+  // $stateProvider.state('courses.students', {
+  // url: "/:courseId",
+  // templateUrl: "partials/courses.students.html",
+  // controller: 'StudentsCtrl'
+  // });
+
+});
